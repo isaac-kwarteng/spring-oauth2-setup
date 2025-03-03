fe16832eb47e4f74402facb17440d93c59c1e968
isaac-kwarteng <kwartzadey1@gmail.com>
2025-03-03 20:00:46 +0000

    Set up home controller for the following routes: - / - /secured

--- src/main/java/_ideas/oauth2/Controllers/HomeController.java
+++ src/main/java/_ideas/oauth2/Controllers/HomeController.java
@@ -1,4 +1,18 @@
 package _ideas.oauth2.Controllers;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "This is home page";
+    }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "This is page secured";
+    }
 }
